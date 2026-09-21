2c1e5833e89b92a9a6c509f9d587d306bc8235ba
mrlordfarquad <144800480+mrlordfarquad@users.noreply.github.com>
2026-09-21 17:37:04 +0500

    Point initial

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,2 +1,20 @@
 public class Point {
+    public final double x;
+    public final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distanceSquared(Point other) {
+        double dx = x - other.x;
+        double dy = y - other.y;
+        return dx * dx + dy * dy;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
